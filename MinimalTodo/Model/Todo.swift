043588a894b8c27a7f6d49394457264672898c25
soham7kulkarni <soham7kulkarni@gmail.com>
2024-02-21 11:55:24 -0800

    Showing Recent 15 Completed Tasks

---
+++ MinimalTodo/Model/Todo.swift
@@ -0,0 +1,18 @@
+//
+//  Todo.swift
+//  MinimalTodo
+//
+//  Created by csuftitan on 2/21/24.
+//
+
+import SwiftUI
+
+struct Todo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Todo()
+}
